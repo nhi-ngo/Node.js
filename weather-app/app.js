6455a9ab06f7e64c5a521a6cec4102f4c08b1d64
nhi-ngo <nhingo.y@gmail.com>
2020-11-06 11:12:01 -0500

    destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,17 +5,21 @@ const forecast = require('./utils/forecast')
 
 const address = process.argv[2]
 
-geocode(address, (error, data) => {
-  if (error) {
-    return console.log(error)
-  }
-
-  forecast(data.lat, data.long, (error, forecastData) => {
+if (!address) {
+  console.log('Please provide an address')
+} else {
+  geocode(address, (error, { lat, long, location }) => {
     if (error) {
       return console.log(error)
     }
 
-    console.log(data.location)
-    console.log(forecastData)
+    forecast(lat, long, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+
+      console.log(location)
+      console.log(forecastData)
+    })
   })
-})
+}
