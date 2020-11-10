b989bd23700baebc622b333850672120e818fb70
nhi-ngo <nhingo.y@gmail.com>
2020-11-09 20:57:25 -0500

    handlebars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,32 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// set up handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// set up static directory
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  // render a view template
+  res.render('index', {
+    title: 'My New Post',
+    body: 'This is my first post!',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    body: 'Accountant turned web developer',
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send('Your weather')
