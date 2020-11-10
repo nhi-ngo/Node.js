1a3f0d2a3b89d3987973191a414307d9c9453a74
nhi-ngo <nhingo.y@gmail.com>
2020-11-10 11:08:17 -0500

    handle 404 view

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -35,7 +35,27 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather')
+  res.render('weather', {
+    forecast: 'It is sunny',
+    location: 'Philadelphia',
+    name: 'Nhi Ngo',
+  })
+})
+
+app.get('/weather/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Nhi Ngo',
+    errorMessage: 'Weather article not found',
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Nhi Ngo',
+    errorMessage: 'Page not found.',
+  })
 })
 
 app.listen(3000, () => {
