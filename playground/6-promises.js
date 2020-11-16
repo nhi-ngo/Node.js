7917f1e5aa01871e9786dd6be360e008eebe58b4
nhi-ngo <nhingo.y@gmail.com>
2020-11-15 21:25:55 -0500

    playground

--- playground/6-promises.js
+++ playground/6-promises.js
@@ -18,3 +18,25 @@ doWorkPromise
 //                              \
 //                               rejected
 //
+
+/* Promise chaining */
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 2000)
+  })
+}
+
+add(1, 1)
+  .then((sum) => {
+    console.log(sum)
+    return add(sum, 4) // return next promise
+  })
+  .then((sum2) => {
+    console.log(sum2)
+  })
+  .catch((e) => {
+    console.log(e)
+  })
